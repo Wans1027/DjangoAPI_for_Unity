20499ef89f1ee48a17fbd0ce0d7852a6f5995ace
Wans1027 <110229966+Wans1027@users.noreply.github.com>
2022-11-24 13:38:36 +0900

    delete exposed secret_key

--- DRF/settings.py
+++ DRF/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '=2dsytsx*vhhsounu7ch#e8zy@p#-41zt&4*hmgkzfla%kyti_'
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
